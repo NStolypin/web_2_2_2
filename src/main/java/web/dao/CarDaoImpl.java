bc9514c5f9dd5252b21a7b13e6e4445fe5b54c04
NStolypin <i@nstolypin.ru>
2024-12-29 14:31:04 +0300

    fix id in List<Car>

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -10,10 +10,10 @@ import web.models.Car;
 public class CarDaoImpl implements CarDao {
     private List<Car> cars = List.of(
             new Car(1, "Lada", 12345),
-            new Car(1, "Geely", 23456),
-            new Car(1, "Mersedes", 34567),
-            new Car(1, "BMW", 45678),
-            new Car(1, "UAZ", 56789));
+            new Car(2, "Geely", 23456),
+            new Car(3, "Mersedes", 34567),
+            new Car(4, "BMW", 45678),
+            new Car(5, "UAZ", 56789));
 
     @Override
     public List<Car> getCars(int count) {
